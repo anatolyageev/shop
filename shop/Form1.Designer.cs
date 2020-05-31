7115b8af8a0b86d156dfbbee644da7dfd6f71b50
Anatolii <anatol1812@gmail.com>
2020-05-31 15:20:13 +0300

    Add project files.

---
+++ shop/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showAllBuers = new System.Windows.Forms.Button();
+            this.showAllSellers = new System.Windows.Forms.Button();
+            this.showAllSales = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showAllBuers
+            // 
+            this.showAllBuers.Location = new System.Drawing.Point(12, 12);
+            this.showAllBuers.Name = "showAllBuers";
+            this.showAllBuers.Size = new System.Drawing.Size(191, 59);
+            this.showAllBuers.TabIndex = 1;
+            this.showAllBuers.Text = "showAllByers";
+            this.showAllBuers.UseVisualStyleBackColor = true;
+            this.showAllBuers.Click += new System.EventHandler(this.showAllByers_Click);
+            // 
+            // showAllSellers
+            // 
+            this.showAllSellers.Location = new System.Drawing.Point(241, 12);
+            this.showAllSellers.Name = "showAllSellers";
+            this.showAllSellers.Size = new System.Drawing.Size(183, 59);
+            this.showAllSellers.TabIndex = 2;
+            this.showAllSellers.Text = "showAllSellers\r\n";
+            this.showAllSellers.UseVisualStyleBackColor = true;
+            this.showAllSellers.Click += new System.EventHandler(this.showAllSellers_Click);
+            // 
+            // showAllSales
+            // 
+            this.showAllSales.Location = new System.Drawing.Point(481, 12);
+            this.showAllSales.Name = "showAllSales";
+            this.showAllSales.Size = new System.Drawing.Size(185, 59);
+            this.showAllSales.TabIndex = 3;
+            this.showAllSales.Text = "showAllSales";
+            this.showAllSales.UseVisualStyleBackColor = true;
+            this.showAllSales.Click += new System.EventHandler(this.showAllSales_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1111, 690);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1790, 1016);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.showAllSales);
+            this.Controls.Add(this.showAllSellers);
+            this.Controls.Add(this.showAllBuers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.showAllByers_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button showAllBuers;
+        private System.Windows.Forms.Button showAllSellers;
+        private System.Windows.Forms.Button showAllSales;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ shop/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shop
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlDataReader reader;
+        private DataTable table;
+        private SqlConnection conn;
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.
+            ConnectionStrings["connectionStr"].
+            ConnectionString;
+            conn.ConnectionString = cs;
+        }
+
+        //private void loadData()
+        //{
+        //    string connectString = "Data Source=localhost;Initial Catalog=shop; Integrated Security=True";
+
+        //    SqlConnection sqlConnection = new SqlConnection(connectString);
+
+        //    sqlConnection.Open();
+
+        //    string query = "SELECT * FROM byers";
+
+        //    SqlCommand command = new SqlCommand(query, sqlConnection);
+
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    List<string[]> data = new List<string[]>();
+
+        //    while (reader.Read())
+        //    {
+        //        data.Add(new string[3]);
+
+        //        data[data.Count - 1][0] = reader[0].ToString();
+        //        data[data.Count - 1][1] = reader[1].ToString();
+        //        data[data.Count - 1][2] = reader[2].ToString();
+
+        //    }
+
+        //    reader.Close();
+        //    sqlConnection.Close();
+
+        //    foreach(string[] s in data)
+        //    {
+        //        Console.WriteLine(s);
+        //        dataGridView1.Rows.Add(s);
+        //    }
+
+        //}
+
+        
+
+        private void showAllByers_Click(object sender, EventArgs e)
+        {
+            showQuery("SELECT * FROM byers");
+
+            //Console.WriteLine("clic all");
+            //string connectString = "Data Source=localhost;Initial Catalog=shop; Integrated Security=True";
+
+            //SqlConnection sqlConnection = new SqlConnection(connectString);
+
+            //sqlConnection.Open();
+
+            //string query = "SELECT * FROM byers";
+
+            //SqlCommand command = new SqlCommand(query, sqlConnection);
+
+            //SqlDataReader reader = command.ExecuteReader();
+
+            //List<string[]> data = new List<string[]>();
+
+            //while (reader.Read())
+            //{
+            //    data.Add(new string[3]);
+
+            //    data[data.Count - 1][0] = reader[0].ToString();
+            //    data[data.Count - 1][1] = reader[1].ToString();
+            //    data[data.Count - 1][2] = reader[2].ToString();
+
+            //}
+
+            //reader.Close();
+            //sqlConnection.Close();
+
+            //foreach (string[] s in data)
+            //{
+            //    Console.WriteLine(s);
+            //    dataGridView1.Rows.Add(s);
+            //}
+        }
+
+        private void showQuery(string query)
+        {
+            try
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.CommandText = query;
+                
+                comm.Connection = conn;
+                dataGridView1.DataSource = null;
+                conn.Open();
+                table = new DataTable();
+                reader = comm.ExecuteReader();
+                int line = 0;
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i <
+                            reader.FieldCount; i++)
+                            {
+                                table.Columns.Add(reader.
+                                GetName(i));
+                            }
+                            line++;
+                        }
+                        DataRow row = table.NewRow();
+                        for (int i = 0; i <
+                        reader.FieldCount; i++)
+                        {
+                            row[i] = reader[i];
+                        }
+                        table.Rows.Add(row);
+                    }
+                } while (reader.NextResult());
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Probably wrong request syntax");
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void showAllSellers_Click(object sender, EventArgs e)
+        {
+            showQuery("SELECT * FROM sellers");
+        }
+
+        private void showAllSales_Click(object sender, EventArgs e)
+        {
+            showQuery("SELECT sl.ID , s.NAME SELLER_NAME, s.LASTNAME SELLER_LASTNAME , " +
+                "b.NAME BYER_NAME , b.LASTNAME BYER_LASTNAME , SUMM , DATE_SELES " +
+                "FROM sales sl, sellers s, byers b " +
+                "where sl.ID_BYER = b.ID   " +
+                "AND sl.ID_SELLER = s.ID");
+        }
+    }
+}
+    
